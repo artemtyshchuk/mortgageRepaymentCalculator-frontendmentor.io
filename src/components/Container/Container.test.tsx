72e19a15f76961df831a4258e7e996d6bb45ee25
Artem Tyshchuk <artemtyschukk@gmail.com>
2024-08-12 10:46:31 +0200

    added the preview pic and tested the container

--- src/components/Container/Container.test.tsx
+++ src/components/Container/Container.test.tsx
@@ -1,3 +1,15 @@
+import { render, screen } from "@testing-library/react";
+
 import { Container } from "./Container";
 
-describe("Container", () => {});
+describe("Container", () => {
+  it("should render with children", () => {
+    const children = <div>test</div>;
+
+    render(<Container children={children} />);
+    expect(screen.getByText("test")).toBeInTheDocument();
+
+    // const { container } = render(<Container>{children}</Container>);
+    // expect(container).toMatchSnapshot();
+  });
+});
